531975e3ec8b048883ed2f8c71a1b8b46d82f820
kvojps <joseferreira.santosjunior@upe.br>
2022-07-01 01:40:37 -0300

    refatorando pasta estudoJs

--- EstudoJs/F-escopo/escopoBloco.js
+++
@@ -1,8 +0,0 @@
-if(true) {
-    const fun = "escopo de bloco const"
-    let fun2 = "escopo de bloco let"
-    var fun3 = "escopo de bloco var"
-}
-console.log(fun)//fun is not defined
-console.log(fun2)//fun is not defined
-console.log(fun3)//escopo de bloco de var
--- EstudoJs/F-escopo/escopoGlobal.js
+++
@@ -1,3 +0,0 @@
-jogada = "pedra"
-//Caso seja chamado fora desse arquivo, não é definido
-var jogada2 = "papel"
